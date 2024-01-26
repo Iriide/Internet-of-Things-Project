463ba0f9c7c84f4156751bed85f2aaec3ba1db05
asiawoj1 <asiawoj1@gmail.com>
2024-01-26 21:20:14 +0100

    OMG IT WORKS

--- Ultrasonic_sensor_oled/main/main.c
+++ Ultrasonic_sensor_oled/main/main.c
@@ -11,7 +11,26 @@
 #include "esp_adc/adc_cali_scheme.h"
 #include "esp_log.h"
 #include "soc/soc_caps.h"
+#include "driver/ledc.h"
 
+
+/*---------------------------------------------------------------
+        Door General Macros
+---------------------------------------------------------------*/
+#define DOOR_OPEN_MAX_DISTANCE_CM 10 // 15cm max
+#define DOOR_BUZZER_MAX_TIMER 30 // 30 * 500ms = 15s
+#define DOOR_CLOSED_MAX_TIMER 5 // 5 * 500ms = 2.5s
+/*---------------------------------------------------------------
+        Buzzer General Macros
+---------------------------------------------------------------*/
+
+#define LEDC_TIMER              LEDC_TIMER_0
+#define LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_OUTPUT_IO          (27) // Define the output GPIO
+#define LEDC_CHANNEL            LEDC_CHANNEL_0
+#define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY               (4095) // Set duty to 50%. ((2 ** 13) - 1) * 50% = 4095
+#define LEDC_FREQUENCY          (500) // Frequency in Hertz. Set frequency at 5 kHz
 
 /*---------------------------------------------------------------
         Display General Macros
@@ -30,10 +49,9 @@
 static int adc_raw[2][10];
 static int voltage[2][10];
 static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
-static void example_adc_calibration_deinit(adc_cali_handle_t handle);
 
 
-    /*---------------------------------------------------------------
+/*---------------------------------------------------------------
         ADC Calibration
 ---------------------------------------------------------------*/
 static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle)
@@ -85,26 +103,46 @@ static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel,
     return calibrated;
 }
 
-static void example_adc_calibration_deinit(adc_cali_handle_t handle)
-{
-#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
-    ESP_LOGI(TAG, "deregister %s calibration scheme", "Curve Fitting");
-    ESP_ERROR_CHECK(adc_cali_delete_scheme_curve_fitting(handle));
-
-#elif ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
-    ESP_LOGI(TAG, "deregister %s calibration scheme", "Line Fitting");
-    ESP_ERROR_CHECK(adc_cali_delete_scheme_line_fitting(handle));
-#endif
-}
 
 /*---------------------------------------------------------------
         Read temperature from ADC
 ---------------------------------------------------------------*/
-void read_raw_data(int* raw, adc_oneshot_unit_handle_t adc1_handle)
+void read_raw_data(float* raw, adc_oneshot_unit_handle_t adc1_handle, adc_cali_handle_t adc1_cali_chan0_handle)
 {
     ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN0, &adc_raw[0][0]));
     ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, adc_raw[0][0]);
-    *raw = adc_raw[0][0];
+    ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_chan0_handle, adc_raw[0][0], &voltage[0][0]));
+    ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, voltage[0][0]);
+    *raw = (voltage[0][0] - 500) / 10.0;
+}
+
+/*---------------------------------------------------------------
+        Init buzzer
+---------------------------------------------------------------*/
+
+static void example_ledc_init(void)
+{
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode       = LEDC_MODE,
+        .timer_num        = LEDC_TIMER,
+        .duty_resolution  = LEDC_DUTY_RES,
+        .freq_hz          = LEDC_FREQUENCY,  // Set output frequency at 5 kHz
+        .clk_cfg          = LEDC_AUTO_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode     = LEDC_MODE,
+        .channel        = LEDC_CHANNEL,
+        .timer_sel      = LEDC_TIMER,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .gpio_num       = LEDC_OUTPUT_IO,
+        .duty           = 0, // Set duty to 0%
+        .hpoint         = 0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
 }
 /*---------------------------------------------------------------
         Read distance and display
@@ -112,10 +150,22 @@ void read_raw_data(int* raw, adc_oneshot_unit_handle_t adc1_handle)
 
 void ultrasonic_oled_test(void *pvParameters)
 {
+    bool isDoorOpen = false;
+    int openTimer = 0;
+    int closedTimer = 0;
+
+
     ultrasonic_sensor_t sensor = {
         .trigger_pin = TRIGGER_GPIO,
         .echo_pin = ECHO_GPIO
     };
+
+    // Set the LEDC peripheral configuration
+    example_ledc_init();
+    // Set duty to 50%
+    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 0));
+    // Update duty to apply the new value
+    
 
     //-------------ADC1 Init---------------//
     adc_oneshot_unit_handle_t adc1_handle;
@@ -167,19 +217,58 @@ void ultrasonic_oled_test(void *pvParameters)
         }
         
         char dist[11];
-        int temp;
-        read_raw_data(&temp, adc1_handle);
+        float temp;
+        read_raw_data(&temp, adc1_handle, adc1_cali_chan0_handle);
         char temperature[9];
 
         ESP_LOGI(TAG, "Distance: %0.04f cm", distance*100);
-        ESP_LOGI(TAG, "Temperature: %0.2f C", temp * 0.0625);
+        ESP_LOGI(TAG, "Temperature: %0.2f C", temp);
+
         sprintf(dist, " %0.04f cm", distance*100);
-        sprintf(temperature, " %0.2f C", temp * 0.0625);
+        sprintf(temperature, " %0.2f C", temp);
+
         ssd1306_display_text(&dev, 0, "---Distance---", 15, true);
         ssd1306_display_text(&dev, 1, dist, 11, false);
         ssd1306_display_text(&dev, 2, "-----Temp-----", 15, true);
         ssd1306_display_text(&dev, 3, temperature, 9, false);
-    
+        ssd1306_display_text(&dev, 5, "-----Door-----", 15, true);
+        if(isDoorOpen){
+            ssd1306_display_text(&dev, 6, "    Open    ", 12, false);
+        } else {
+            ssd1306_display_text(&dev, 6, "    Closed    ", 14, false);
+        }
+
+
+       if (distance * 100 >= DOOR_OPEN_MAX_DISTANCE_CM) {
+            // Door is open
+            if (!isDoorOpen) {
+                // Door was previously closed, now open
+                isDoorOpen = true;
+                openTimer = 0;
+            } else {
+                // Door remains open
+                if (openTimer >= DOOR_BUZZER_MAX_TIMER) {
+                    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
+                    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+                } else {
+                    openTimer++;
+                }
+            }
+            closedTimer = 0; // Reset closed timer
+        } else {
+            // Door is closed
+            if (isDoorOpen) {
+                // Door was open, now appears closed
+                if (closedTimer >= DOOR_CLOSED_MAX_TIMER) {
+                    // Confirm door is closed
+                    isDoorOpen = false;
+                    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 0));
+                    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+                } else {
+                    closedTimer++;
+                }
+            }
+        }
         vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
